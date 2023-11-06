f967e9298124c253f95a0a817999cbd58783e200
Jacob-32587 <jgbs32587@gmail.com>
2023-11-05 19:18:24 -0500

    Removed old files

--- windows/int.ps1
+++
@@ -1,5 +0,0 @@
-winget install Microsoft.WindowsTerminal Microsoft.PowerToys vscodium
-winget install -e --id Microsoft.VisualStudio.2022.BuildTools --override "--passive --wait --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended"
-./settings.ps1
-Invoke-WebRequest -Uri https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe -OutFile rustup-init.exe
-Invoke-expression ".\settings.ps1"
